5d901cdeec6d0c30a1055821767f3d5ec28b48f7
Henry Westphal <westhen@umich.edu>
2025-07-04 19:37:28 -0400

    Initial writing

--- SmartPark/SmartPark/ContentView.swift
+++ SmartPark/SmartPark/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import MapKit
+
+extension CLLocationCoordinate2D {
+    static var coffee: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: 37.3318, longitude: -121.8863)
+    }
+    static var oakland: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: 37.8044, longitude: -122.2711)
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map {
+            Annotation("Coffee", coordinate: .coffee) {
+                Image(systemName: "cup.and.saucer.fill")
+                    .padding(4)
+                    .foregroundColor(.white)
+                    .background(.indigo)
+                    .cornerRadius(4.0)
+            }
+            
+            Annotation("Oakland", coordinate: .oakland) {
+                Image(systemName: "city.fill")
+                    .padding(4)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(4.0)
+            }
         }
-        .padding()
     }
 }
 
