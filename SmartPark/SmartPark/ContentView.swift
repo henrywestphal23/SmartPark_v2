5f91544dc23050824525c12e119ea9a24b8f39e0
Henry Westphal <westhen@umich.edu>
2025-07-04 17:50:40 -0400

    Initial commit v2

---
+++ SmartPark/SmartPark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartPark
+//
+//  Created by Henry Westphal on 7/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartPark/SmartPark/SmartParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartParkApp.swift
+//  SmartPark
+//
+//  Created by Henry Westphal on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
